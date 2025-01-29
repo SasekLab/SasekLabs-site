bae69e8c03f1eef3beff167964d916170fba973e
Ewurama12 <124101004+Kerry-Jilak@users.noreply.github.com>
2025-01-29 14:41:11 +0000

    changes to index.html and home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -32,9 +32,7 @@ export function Home() {
                 Your story starts with us.
               </Typography>
               <Typography variant="lead" color="white" className="opacity-80">
-                This is a simple example of a Landing Page you can build using
-                Material Tailwind. It features multiple components based on the
-                Tailwind CSS and Material Design by Google.
+              ⚡Say goodbye to oudated systems and low productivity. Our cutting-edge technology transforms your work-saving you time and of course-money🤑
               </Typography>
             </div>
           </div>
