6a4cc15a64db3ac0ab1b835e25a09d67b221f7cb
neweracy <kwakansah2@gmail.com>
2025-01-29 14:57:42 +0000

    Refactor home page and navbar; update routes by removing unused components

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { Home, Profile, SignIn, SignUp } from "@/pages";
+import { Home, Profile} from "@/pages";
 
 export const routes = [
   {
@@ -11,22 +11,22 @@ export const routes = [
     path: "/profile",
     element: <Profile />,
   },
-  {
-    name: "Sign In",
-    path: "/sign-in",
-    element: <SignIn />,
-  },
-  {
-    name: "Sign Up",
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    name: "Docs",
-    href: "https://www.material-tailwind.com/docs/react/installation",
-    target: "_blank",
-    element: "",
-  },
+  // {
+  //   name: "Sign In",
+  //   path: "/sign-in",
+  //   element: <SignIn />,
+  // },
+  // {
+  //   name: "Sign Up",
+  //   path: "/sign-up",
+  //   element: <SignUp />,
+  // },
+  // {
+  //   name: "Docs",
+  //   href: "https://www.material-tailwind.com/docs/react/installation",
+  //   target: "_blank",
+  //   element: "",
+  // },
 ];
 
 export default routes;
