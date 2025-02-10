5f5df2d0830c57db01ee3ed856a38e7abd39d982
kerryjilak <kerryjilak12@gmail.com>
2025-02-10 10:27:08 +0000

    Update core values

--- src/data/contact-data.js
+++ src/data/contact-data.js
@@ -5,34 +5,35 @@ import {
   WrenchIcon,
   StarIcon,
   HeartIcon,
+  UsersIcon,
 } from "@heroicons/react/24/solid";
 
 export const contactData = [
   {
-    title: "Hardwork",
-    icon: BriefcaseIcon,
-    description:
-      "Hardwork is at the core of our team. We go all out to ensure that you,our customer, get exactly what you wanted and even more of ",
-  },
-  {
-    title: "Innovation",
-    icon: StarIcon,
-    description:
-      "We are dedicated to staying at the forefront of technology. We embrace new ideas, explore cutting-edge solutions, and constantly innovate to provide you with the most effective services to propel your business forward.",
-  },
-  {
     title: "Passion",
     icon: HeartIcon,
     description:
-      "We are fueled by a genuine passion for technology and a deep enthusiasm for solving problems. This passion drives us to go the extra mile and deliver solutions that truly make a difference for our clients.",
+    "We are fueled by a genuine passion for technology and a deep enthusiasm for solving problems. This passion drives us to go the extra mile and deliver solutions that truly make a difference for our clients."
+  },
+  {
+    title: "Collaboration",
+    icon:UsersIcon,
+    description:
+      "We believe in the power of teamwork, both internally and with our clients. By collaborating closely, sharing ideas, and working together, we achieve greater innovation and create solutions that are perfectly aligned with your needs."
   },
   {
     title: "Quality",
-    icon: WrenchIcon,
+    icon:WrenchIcon ,
     description:
       "We are committed to delivering the highest standards of quality in everything we do. From \
       AI services to software development, we meticulously focus on precision, reliability, and \
-      excellence to ensure your satisfaction.",
+      excellence to ensure your satisfaction",
+  },
+  {
+    title: "Innovation",
+    icon:StarIcon ,
+    description:
+      "We are dedicated to staying at the forefront of technology. We embrace new ideas, explore cutting-edge solutions, and constantly innovate to provide you with the most effective services to propel your business forward.",
   },
 ];
 
